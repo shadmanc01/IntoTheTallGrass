8bd62c1657d0dfb33fbc86b257de2eaf0f80d332
Shadman <shadmanchowdhury01@gmail.com>
2022-08-30 14:45:42 -0400

    keyEvents added need player

--- game.js
+++ game.js
@@ -41,4 +41,23 @@ scene("game", () => {
 
 })
 
+// keyEvents
+
+const playerSpeed = 120;
+const jumpForce = 360;
+
+keyDown('left', () => {
+    player.move(-playerSpeed, 0);
+})
+
+keyDown('right', () => {
+    player.move(+playerSpeed, 0);
+})
+
+keyDown('space', () => {
+    if(player.grounded()) {
+        player.jump(jumpForce)
+    }
+})
+
 start("game")
