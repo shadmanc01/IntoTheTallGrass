8b6d111dce73adc9e26d4091213528c0ea7ae6be
clopez11224 <christopherlopez000@gmail.com>
2022-08-31 11:38:25 -0400

    Unable to fix bug that allow enemy to walk through solid objects

--- game.js
+++ game.js
@@ -111,10 +111,32 @@ const player = add([
     scoreLabel.value++
     scoreLabel.text = scoreLabel.value
   })
-
+  function patrol(distance = 100, speed = 50, dir = 1) {
+    return {
+      id: "patrol",
+      require: ["pos", "area",],
+      startingPos: vec2(0, 0),
+      add() {
+        this.startingPos = this.pos;
+        this.on("collide", (obj, side) => {
+          if (side === "left" || side === "right") {
+            dir = -dir;
+          }
+        });
+      },
+      update() {
+        if (Math.abs(this.pos.x - this.startingPos.x) >= distance) {
+          dir = -dir;
+        }
+        this.move(speed * dir, 0);
+      },
+    };
+  }
   action('dangerous', (d) => {
-    d.move(-ENEMY_SPEED, 0)
+      // d.move(-ENEMY_SPEED, 0)
+    patrol()
   })
+
 
   let isJumping = true;
 
